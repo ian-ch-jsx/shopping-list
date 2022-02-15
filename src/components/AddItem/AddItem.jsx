410deb3c8f84852ceaa9252ad1b77ea5c0817908
Ian Christiansen <ianc.songdog@gmail.com>
2022-02-15 14:07:18 -0800

    itemList maps through item

--- src/components/AddItem/AddItem.jsx
+++ src/components/AddItem/AddItem.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react';
 
-export default function AddItem() {
+export default function AddItem({ onAddItem }) {
   //newitem usestate
   const [newItem, setNewItem] = useState('');
 
   //handleSubmit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setNewItem('');
+    onAddItem(newItem);
+  };
 
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input placeholder="item" value={newItem} onChange={(e) => setNewItem(e.target.value)} />
         <button type="submit">Add Item</button>
       </form>
--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import Item from '../Item/Item';
 
-export default function ItemList() {
+export default function ItemList({ onChangeItem, onDeleteItem, items }) {
   return (
     <div>
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+            <Item item={item} onChange={onChangeItem} onDelete={onDeleteItem} />
+          </li>
+        ))}
+      </ul>
       <Item />
-      {/* // item map */}
     </div>
   );
 }
