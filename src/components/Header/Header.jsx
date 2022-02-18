8f241cb1379b5c74ff7b78d9cfe257f28ff09e37
Ian Christiansen <ianc.songdog@gmail.com>
2022-02-18 11:13:19 -0800

    addeed clear cart

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,14 +2,15 @@ import './Header.css';
 import { useList } from '../../context/ListContext';
 
 export default function Header() {
-  const { items } = useList();
+  const { items, handleClearItems } = useList();
+
   const message =
     items.length === 1 ? `${items.length} item in cart` : `${items.length} items in cart`;
   return (
     <>
       <header>
         <div>
-          <p>{message}</p>
+          <p>{message}</p> <button onClick={handleClearItems}>clear cart</button>
         </div>
       </header>
     </>
--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,12 +1,12 @@
 import Item from '../Item/Item';
 
-export default function ItemList({ onEditItem, onDeleteItem, items }) {
+export default function ItemList({ onEditItem, onDeleteItem, items, onClearItems }) {
   return (
     <div className="items-container">
       <ul>
         {items.map((item) => (
           <li key={item.id}>
-            <Item item={item} onEdit={onEditItem} onDelete={onDeleteItem} />
+            <Item item={item} onEdit={onEditItem} onDelete={onDeleteItem} onClear={onClearItems} />
           </li>
         ))}
       </ul>
